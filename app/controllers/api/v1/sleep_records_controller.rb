d2c96e70f97c3a8538f4dd5b33a6db5b15216831
agamstawn <setiawanagam@gmail.com>
2025-02-05 22:28:49 +0700

    add sleep records contoller to get and create sleep records

---
+++ app/controllers/api/v1/sleep_records_controller.rb
@@ -0,0 +1,27 @@
+class Api::V1::SleepRecordsController < ApplicationController
+  before_action :set_user, only: [:index, :create]
+  
+  def index
+    sleep_records = @user.sleep_records.order(created_at: :desc)
+    render json: sleep_records, status: :ok
+  end
+
+  def create
+    sleep_record = @user.sleep_records.new(sleep_record_params)
+    if sleep_record.save
+      render json: sleep_record, status: :created
+    else
+      render json: { errors: sleep_record.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def sleep_record_params
+    params.require(:sleep_record).permit(:sleep_time, :wake_time)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [] do
+        resources :sleep_records, only: [:index, :create]
+      end
+      
+    end
+  end
+  
 end
