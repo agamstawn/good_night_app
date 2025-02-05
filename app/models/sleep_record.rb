a6b39821a57ca165d7058cf21ca17832f3d3469b
agamstawn <setiawanagam@gmail.com>
2025-02-05 22:13:33 +0700

    - update user model association - add sleep time and wake time validation

--- app/models/sleep_record.rb
+++ app/models/sleep_record.rb
@@ -1,3 +1,20 @@
 class SleepRecord < ApplicationRecord
   belongs_to :user
+
+  validates :sleep_time, presence: true
+  validate :wake_time_after_sleep_time
+
+  def sleep_duration
+    return 0 unless wake_time
+    (wake_time - sleep_time) / 3600.0
+  end
+
+  private
+
+  def wake_time_after_sleep_time
+    if wake_time.present? && wake_time <= sleep_time
+      errors.add(:wake_time, "must be after sleep time")
+    end
+  end
+
 end
