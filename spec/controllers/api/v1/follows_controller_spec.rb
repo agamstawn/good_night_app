7142b6ef0b442afe934b93941f4088eb6a1316be
agamstawn <setiawanagam@gmail.com>
2025-02-06 22:19:50 +0700

    add follow controller spec

---
+++ spec/controllers/api/v1/follows_controller_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::FollowsController, type: :controller do
+  let(:follower) { User.create(name: "Goku") }
+  let(:followed) { User.create(name: "Gohan") }
+
+  describe "POST #create" do
+    context "when following a user" do
+      before do
+        post :create, params: { follower_id: follower.id, followed_id: followed.id }
+        follower.reload
+      end
+
+      it "returns a success message" do
+        expect(JSON.parse(response.body)["message"]).to eq("Now following #{followed.name}")
+      end
+
+      it "returns a 200 status" do
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+
+end
