d55081d6dc75155227225155f1fd8edc71547ac8
agamstawn <setiawanagam@gmail.com>
2025-02-06 22:13:00 +0700

    add follow and unfollow feature

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
       resources :users, only: [] do
         resources :sleep_records, only: [:index, :create]
       end
-      
+      resources :follows, only: [:create, :destroy]
     end
   end
   
