130fa564b2a2a9eff7cbbf3353268d6f35579acb
agamstawn <setiawanagam@gmail.com>
2025-02-06 21:40:54 +0700

    add follow asscociation to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_many :sleep_records, dependent: :destroy
+
+  has_many :followers, foreign_key: :followed_id, class_name: "Follow"
+  has_many :followed_users, foreign_key: :follower_id, class_name: "Follow"
 end
