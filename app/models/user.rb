a2918fc000543e6e459c6579920300941085791a
agamstawn <setiawanagam@gmail.com>
2025-02-08 12:00:56 +0700

    fix user follow association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   has_many :sleep_records, dependent: :destroy
 
-  has_many :followers, foreign_key: :followed_id, class_name: "Follow"
-  has_many :followed_users, foreign_key: :follower_id, class_name: "Follow"
+  has_many :active_follows, class_name: "Follow", foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :active_follows, source: :followed
+
+  has_many :passive_follows, class_name: "Follow", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :passive_follows, source: :follower 
+
+
 end
